c08ee85cfc92d3489997e7cb2d31550f5965490a
Jeremy.z <jeremyzap2001@gmail.com>
2026-04-27 21:05:22 -0700

    Save game implementation
    Save game code implemented. Accounts for current level, inventory, and stats.

---
+++ scripts/save_game.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+
+var config = ConfigFile.new()
+
+#saves information pertaining to current progress
+func save(level, stats, inventory):
+	config.set_value("level","current",level)
+	config.set_value("stats","current",stats)
+	config.set_value("inventory","current",inventory)
+	config.save("res://scenes/savegame.cfg")
+	
+#on continue, recovers save information from savegame file
+func recover():
+	var err = config.load("res://scenes/savegame.cfg")
+	if err == OK:
+		get_tree().change_scene_to_file(config.get_value("level","current"))

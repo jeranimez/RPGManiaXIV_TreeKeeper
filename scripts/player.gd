6eb5ed5639b4b9ad13e21021dafcadb573f3a842
Jeremy.z <jeremyzap2001@gmail.com>
2026-04-27 23:39:02 -0700

    Implemented basic movement
    Implemented movement based on key and mouse input.

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,28 +1,30 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 10.0
+var click_position = position
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle Jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-	#	velocity.y = JUMP_VELOCITY
-
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	
+	if Input.is_action_just_pressed("left_click"):
+		click_position = get_global_mouse_position()
+	
+	if Input.is_action_pressed("left"):
+		click_position = position
+		position.x -= SPEED
+	elif Input.is_action_pressed("right"):
+		click_position = position
+		position.x += SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if click_position.x == 0:
+			click_position = position
+		if position.x < click_position.x - 10:
+			position.x += SPEED
+		elif position.x > click_position.x + 10:
+			position.x -= SPEED
 
 	move_and_slide()
--- scripts/save_game.gd
+++ scripts/save_game.gd
@@ -4,10 +4,11 @@ extends Node2D
 var config = ConfigFile.new()
 
 #saves information pertaining to current progress
-func save(level, stats, inventory):
+func save(level, stats, inventory, dialogue):
 	config.set_value("level","current",level)
 	config.set_value("stats","current",stats)
 	config.set_value("inventory","current",inventory)
+	config.set_value("dialogue","current",dialogue)
 	config.save("res://scenes/savegame.cfg")
 	
 #on continue, recovers save information from savegame file
